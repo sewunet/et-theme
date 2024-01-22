c1150dbafb9e3a5ef210825be5befa7b686b05c6
sewunet <sewunet92@gmail.com>
2024-01-22 11:11:11 +0300

    Fix Branding

--- et_theme/hooks.py
+++ et_theme/hooks.py
@@ -18,7 +18,7 @@ app_license = "mit"
 # web_include_js = "/assets/et_theme/js/et_theme.js"
 
 # include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "et_theme/public/scss/et_theme"
+website_theme_scss = "et_theme/public/scss/et_theme"
 
 # include js, css files in header of web form
 # webform_include_js = {"doctype": "public/js/doctype.js"}
